391aa44e09399f5940fbe24c96be084d01aba48c
raawaa <wenjie_yu@yeah.net>
2015-10-28 16:39:28 +0800

    async

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var vo = require('vo');
 var Nightmare = require('nightmare');
 var cheerio = require('cheerio');
 var request = require('superagent');
+var async = require('async');
 
 
 const baseUrl = 'http://javbus.in';
-var pageIndex = 55464565654;
+var pageIndex = 1;
 
 // request.get(baseUrl + '/page/' + pageIndex).redirects(2).end(function(err,res){
 // 	if(err) console.log(err.status);
@@ -14,14 +15,17 @@ var pageIndex = 55464565654;
 // });
 
 
-var singlePage = vo(getPage, getPageLinks, getItems)(baseUrl + '/page/' + pageIndex, function(err, res) {
+var singlePage = vo(getPage, getPageLinks)(baseUrl + '/page/' + (pageIndex===1?'':pageIndex), function(err, res) {
 	if (err) throw err;
-	console.log(res);
+	getItems(res);
+	console.log('END!!');
+
 });
 
 function getPage(url, next) {
-	request.get(url).redirects(2).end(function(err, res) {
-		// if (err){ console.log(err.status); return next(err)};
+	console.log('get page: ',url);
+	request.get(url).redirects(3).end(function(err, res) {
+		if (err){ console.log(err.status); return next(err)};
 		console.log('=================');
 		return next(null, res.text);
 	});
@@ -40,12 +44,33 @@ function getPageLinks(html) {
 
 
 
-function* getItems(links) {
-	while (links.length) {
-		let link = links.pop();
-		let response = yield request.get(link);
-		var $ = cheerio.load(response.text);
-		let script = $('script', 'body').eq(3).html();
-		console.log(script);
+// function* getItems(links) {
+// 	while (links.length) {
+// 		let link = links.pop();
+// 		let res = yield request.get(link);
+// 		var $ = cheerio.load(res.text);
+// 		let script = $('script', 'body').eq(3).html();
+// 		console.log(script);
+// 	}
+// }
+
+function getItems(links) {
+
+	async.mapSeries(links, getItem, function(err,res){
+		if(err) throw err;
+		console.log(res);
+	});
+
+	function getItem(link, done) {
+		console.log(link);
+		request.get(link).end(function(err, res) {
+			console.log(link);
+			if (err) done(err);
+			let $ = cheerio.load(res.text);
+			let script = $('script', 'body').eq(3).html();
+			console.log(script);
+			done(null,script);
+		});
 	}
+
 }
