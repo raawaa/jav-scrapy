965a5ba0beb7fba634b99b12570a29028c2389f4
raawaa <wenjie_yu@yeah.net>
2015-10-28 16:07:10 +0800

    fetch one page

--- index.js
+++ index.js
@@ -1,65 +1,51 @@
 'use strict';
 var vo = require('vo');
 var Nightmare = require('nightmare');
+var cheerio = require('cheerio');
+var request = require('superagent');
 
-var option = {
-	show: true
-};
-var nightmare = Nightmare();
 
-// control flow using vo library
-vo(run)(function(err, res) {
+const baseUrl = 'http://javbus.in';
+var pageIndex = 55464565654;
+
+// request.get(baseUrl + '/page/' + pageIndex).redirects(2).end(function(err,res){
+// 	if(err) console.log(err.status);
+// 	console.log(res.status);
+// });
+
+
+var singlePage = vo(getPage, getPageLinks, getItems)(baseUrl + '/page/' + pageIndex, function(err, res) {
 	if (err) throw err;
+	console.log(res);
 });
 
-// main task
-function * run() {
-	let pageIndex = 1;
-	while (pageIndex) {
-		let links = yield getLinks('http://javbus.in/page/' + pageIndex);
-		if (links.length == 0) {
-			return yield nightmare.end();
-		}
-		while (links.length) {
-			let link = links.pop();
-			let magnets = yield * getItemMagnet(link);
-			if (magnets) console.log(magnets);
-		}
-		pageIndex++;
+function getPage(url, next) {
+	request.get(url).redirects(2).end(function(err, res) {
+		// if (err){ console.log(err.status); return next(err)};
+		console.log('=================');
+		return next(null, res.text);
+	});
+}
+
+function getPageLinks(html) {
+	let $ = cheerio.load(html);
+	let anchors = $('#item-frame');
+	let links = [];
+	anchors.each(function(i, elem) {
+		let url = $(this).children('a').first().attr('href');
+		links.push(url);
+	});
+	return Promise.resolve(links);
+}
+
+
+
+function* getItems(links) {
+	while (links.length) {
+		let link = links.pop();
+		let response = yield request.get(link);
+		var $ = cheerio.load(response.text);
+		let script = $('script', 'body').eq(3).html();
+		console.log(script);
 	}
-	return yield nightmare.end();
 }
-
-// get item links in one page
-function getLinks(pageUrl) {
-	// get all item links in one page
-	let anchors = nightmare.goto(pageUrl).evaluate(function() {
-		var elems = document.querySelectorAll('#item-frame>a');
-		var anchors = [];
-		for (var i = 0, a; a = elems[i]; i++) {
-			anchors.push(a.getAttribute('href'));
-		}
-		return anchors;
-	});
-	return anchors;
-}
-
-// get magnets links in item detail page
-function * getItemMagnet(url) {
-	//console.log('========= getItemMagnet ===========');
-	console.log(url);
-	return yield nightmare.goto(url).wait('#magnet-table>tr').evaluate(function() {
-		//var anchors = document.querySelectorAll('#magnet-table>tr td>a[data-message=magnet]');
-		//var magnets = [];
-		//for (var i = 0, a; a = anchors[i]; i++) {
-		//magnets.push(a.getAttribute('href'));
-		//}
-		//return magnets;
-        
-		// only fetch the first magnet
-		var anchor = document.querySelector('#magnet-table>tr a[data-message=magnet]');
-		if (anchor) return anchor.getAttribute('href');
-		else return null;
-	});
-}
-
