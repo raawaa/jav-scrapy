f8d24e0e1b8d0a42d8deafd95c3a02781c7b46b8
raawaa <wenjie_yu@yeah.net>
2015-10-30 10:18:18 +0800

    设置抓取的磁链上限

--- index.js
+++ index.js
@@ -15,25 +15,31 @@ var noop = function noop() {};
 const baseUrl = 'http://www.javbus.in';
 var pageIndex = 1;
 var currentPageHtml = null;
-var parallel;
-var timeout;
 
 
 program
 	.version('0.1.0')
 	.usage('[options]')
-	.option('-p, --parallel <num>', '设置抓取并发连接数', 2)
-    .option('-t, --timeout <num>','自定义连接超时时间(毫秒)',10000)
+	.option('-p, --parallel <num>', '设置抓取并发连接数，默认值：2', 2)
+	.option('-t, --timeout <num>', '自定义连接超时时间(毫秒)。默认值：10000', 10000)
+	.option('-l,  --limit <num>', '设置抓取影片的数量上限，0为抓取全部影片。默认值：0）', 0)
 	.parse(process.argv);
 
 //if (program.parallel) {
-parallel = program.parallel;
-timeout = program.timeout;
+var parallel = parseInt(program.parallel);
+var timeout = parseInt(program.timeout);
+var count = parseInt(program.limit);
+var hasLimit = !(count === 0);
 //}
 
-console.log('========== 获取资源站点：%s =========='.green, baseUrl);
-console.log('并行连接数：%d       连接超时设置：%d秒'.green, parallel, timeout/1000.0);
+console.log('========== 获取资源站点：%s =========='.green.bold, baseUrl);
+console.log('并行连接数：%d       连接超时设置：%d秒'.green.bold, parallel, timeout / 1000.0);
 
+/****************************
+ *****************************
+ **** MAIN LOOP START ! ******
+ ****************************
+ ****************************/
 async.during(
 	pageExist,
 	// when page exist
@@ -43,16 +49,24 @@ async.during(
 		async.waterfall(
 			pageTasks,
 			function(err, result) {
-                pageIndex++;
+				pageIndex++;
 				if (err) return callback(err);
 				callback(null);
 			});
 	},
 	// FINALLY
 	function(err) {
-        if(err)
-            console.log('抓取过程终止：%s', err.message);
+		if (err)
+			return console.log('抓取过程终止：%s', err.message);
+		if (hasLimit && count < 1)
+			console.log('已抓取%s个磁链，本次抓取完毕，等待未响应的异步请求...'.green.bold, program.limit);
 	});
+
+/****************************
+ *****************************
+ **** MAIN LOOP END ! ******
+ ****************************
+ ****************************/
 
 
 function parseLinks(next) {
@@ -77,10 +91,15 @@ function getMagnet(links, next) {
 		links,
 		parallel,
 		function(link, index, callback) {
+			// console.log('count: %d'.yellow, count);
+
 			request
 				.get(link)
 				.timeout(timeout)
 				.end(function(err, res) {
+					if (hasLimit && count < 1) {
+						return callback(new Error('limit'));
+					};
 					if (err) {
 						console.error('番号%s页面获取失败：%s'.red, link.split('/').pop(), err.message);
 						return callback(null);
@@ -94,6 +113,10 @@ function getMagnet(links, next) {
 						.set('Referer', 'http://www.javbus.in/SCOP-094')
 						.timeout(timeout)
 						.end(function(err, res) {
+							debugger;
+							if (hasLimit && count < 1) {
+								return callback(new Error('limit'));
+							};
 							//console.log('bingo'.blue);
 							if (err) {
 								console.error('番号%s磁链获取失败: %s'.red, link.split('/').pop(), err.message);
@@ -104,19 +127,23 @@ function getMagnet(links, next) {
 							let anchor = $('[onclick]').first().attr('onclick');
 							if (anchor) {
 								anchor = /\'(magnet:.+?)\'/g.exec(anchor)[1];
-								console.log(anchor);
+								console.log(anchor.gray);
+								count--;
 							}
 							return callback(null);
 						});
 				});
 		},
 		function(err) {
+			debugger;
+			if (err && err.message === 'limit')
+				return next();
 			if (err) {
 				throw err;
 				return next(err);
 			};
 			console.log('===== 第%d页处理完毕 ====='.green, pageIndex);
-            console.log();
+			console.log();
 			return next();
 		});
 
@@ -140,11 +167,11 @@ function getMagnet(links, next) {
 
 
 function pageExist(callback) {
-	// body...
+	if (hasLimit && count < 1) return callback();
 	let url = baseUrl + (pageIndex === 1 ? '' : '/page/' + pageIndex);
 	// console.log(url);
 	console.log('获取第%d页中的影片链接...'.green, pageIndex);
-    let retryCount = 1;
+	let retryCount = 1;
 	async.retry(3,
 		function(callback, result) {
 			request
@@ -152,30 +179,30 @@ function pageExist(callback) {
 				.accept('text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
 				.set('Accept-Encoding', 'gzip, deflate')
 				.set('Connection', 'keep-alive')
-                .timeout(timeout)
+				.timeout(timeout)
 				.redirects(2)
 				.end(function(err, res) {
 					// console.log(res.status)
 					if (err) {
-                        if(err.status === 404) {
-                            console.error('已抓取完所有页面,StatusCode:', err.status);
-                            return callback(null);
-                        }else{
-                            retryCount++;
-                            console.error('第%d页页面获取失败：%s'.red, pageIndex, err.message);
-                            console.error('...进行第%d次尝试...'.red, retryCount);
-                            return callback(err);
-                        }
+						if (err.status === 404) {
+							console.error('已抓取完所有页面,StatusCode:', err.status);
+							return callback(null);
+						} else {
+							retryCount++;
+							console.error('第%d页页面获取失败：%s'.red, pageIndex, err.message);
+							console.error('...进行第%d次尝试...'.red, retryCount);
+							return callback(err);
+						}
 					}
 					currentPageHtml = res.text;
 					callback(null, res);
 				});
 		},
 		function(err, res) {
-            retryCount = 3;
+			retryCount = 3;
 			if (err) {
 				return callback(err);
-			} 
-            callback(null, res.ok);
+			}
+			callback(null, res.ok);
 		});
 }
