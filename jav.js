675a1b4a7404759ddae9d315003612c2f8cab62b
raawaa <wenjie_yu@yeah.net>
2019-05-14 20:19:56 +0800

    Update site url; and npm audit.

--- jav.js
+++ jav.js
@@ -15,7 +15,7 @@ const _ = require('lodash');
 // global var
 
 const VERSION = require('./package.json').version;
-const baseUrl = 'https://www.3ubdxu00l1lkcjoz5n.com/'
+const baseUrl = 'https://www.buscdn.life/'
 const searchUrl = '/search';
 var pageIndex = 1;
 var currentPageHtml = null;
