0144b8bd22a857214792ac85637ca5a421ecb598
raawaa <wenjie_yu@yeah.net>
2025-04-28 16:53:20 +0800

    feat(parser): 优化解析器功能并完善注释
    1. 重构了parseMagnet方法，改进磁力链接解析逻辑：
       - 添加了详细的日志输出
       - 优化了大小比较算法
       - 增加了错误处理
    
    2. 为所有方法添加了完整的JSDoc注释，包括：
       - 方法描述
       - 参数说明
       - 返回值说明
       - 属性说明(对于返回对象)
    
    3. 代码结构优化：
       - 统一了代码风格
       - 提取了重复的正则表达式匹配逻辑
       - 改进了变量命名
    
    4. 功能增强：
       - 增加了磁力链接去重处理
       - 支持GB/MB单位自动转换
       - 添加了空值检查

--- src/core/parser.js
+++ src/core/parser.js
@@ -3,15 +3,35 @@ const RequestHandler = require('./requestHandler');
 
 class Parser {
 
+  /**
+   * 构造函数
+   * @param {Object} config - 配置对象
+   */
   constructor(config) {
     this.config = config;
   }
 
+  /**
+   * 解析页面中的电影链接
+   * @param {string} html - 页面HTML内容
+   * @returns {Array<string>} 电影详情页链接数组
+   */
   static parsePageLinks(html) {
     const $ = require('cheerio').load(html);
     return $('a.movie-box').map((i, el) => $(el).attr('href')).get();
   }
 
+  /**
+   * 解析电影元数据
+   * @param {string} html - 电影详情页HTML内容
+   * @returns {Object} 包含电影元数据的对象
+   * @property {string} gid - 电影ID
+   * @property {string} uc - 未知参数
+   * @property {string} img - 封面图片URL
+   * @property {string} title - 电影标题
+   * @property {Array<string>} category - 电影分类数组
+   * @property {Array<string>} actress - 女演员数组
+   */
   static parseMetadata(html) {
     const $ = require('cheerio').load(html);
     const script = $('script', 'body').eq(2).html();
@@ -31,95 +51,52 @@ class Parser {
   }
 
   /**
-   * 解析 HTML 内容中的分类信息。
-   * @param {string} html - 需要解析的 HTML 字符串。
-   * @returns {Array<string>} - 包含所有分类名称的数组。
+   * 解析电影分类
+   * @param {string|Object} html - HTML内容或cheerio对象
+   * @returns {Array<string>} 电影分类数组
    */
   static parseCategories(html) {
-    // 使用 cheerio 库加载 HTML 内容，以便使用类似 jQuery 的语法进行元素选择
     const $ = require('cheerio').load(html);
-    // 选择所有 <span class="genre"> 元素下的 <label> 元素内的 <a> 元素，
-    // 并将这些 <a> 元素的文本内容提取出来，最终返回一个包含所有文本内容的数组
     return $('span.genre label a').map((i, el) => $(el).text()).get();
-
   }
 
+  /**
+   * 解析女演员信息
+   * @param {string|Object} html - HTML内容或cheerio对象
+   * @returns {Array<string>} 女演员数组
+   */
   static parseActress(html) {
     const $ = require('cheerio').load(html);
     return $('span.genre[onmouseover] a').map((i, el) => $(el).text()).get();
   }
+  /**
+   * 解析磁力链接
+   * @param {Object} metadata - 电影元数据对象
+   * @returns {Promise<string|null>} 最大的磁力链接或null
+   */
   async parseMagnet(metadata) {
-    // 使用 RequestHandler 向指定 URL 发送请求
     const url = `https://www.fanbus.ink/ajax/uncledatoolsbyajax.php?gid=${metadata.gid}&lang=zh&img=${metadata.img}&uc=${metadata.uc}&floor=880`;
     const requestHandler = new RequestHandler(this.config);
     const response = await requestHandler.getXMLHttpRequest(url);
 
-    // response.body 的内容是这样的：
-    // "       \r\n            <tr onmouseover=\"this.style.backgroundColor='#F4F9FD';this.style.cursor='pointer';\" onmouseout=\"this.style.backgroundColor='#FFFFFF'\" height=\"35px\" style=\" border-top:#DDDDDD solid 1px\">\r\n                <td width=\"70%\" onclick=\"window.open('magnet:?xt=urn:btih:C081C69939076EC6F0396B13AE926AB090AD8C39&dn=FNEW-009','_self')\">\r\n                \t<a style=\"color:#333\" rel=\"nofollow\" title=\"滑鼠右鍵點擊並選擇【複製連結網址】\" href=\"magnet:?xt=urn:btih:C081C69939076EC6F0396B13AE926AB090AD8C39&dn=FNEW-009\">\r\n                \tFNEW-009 <a class=\"btn btn-mini-new btn-primary disabled\" title=\"包含高清HD的磁力連結\">高清</a>                \t</a>\r\n                </td>\r\n                <td style=\"text-align:center;white-space:nowrap\" onclick=\"window.open('magnet:?xt=urn:btih:C081C69939076EC6F0396B13AE926AB090AD8C39&dn=FNEW-009','_self')\">\r\n                \t<a style=\"color:#333\" rel=\"nofollow\" title=\"滑鼠右鍵點擊並選擇【複製連結網址】\" href=\"magnet:?xt=urn:btih:C081C69939076EC6F0396B13AE926AB090AD8C39&dn=FNEW-009\">\r\n                \t5.87GB                \t</a>\r\n                </td>\r\n                <td style=\"text-align:center;white-space:nowrap\" onclick=\"window.open('magnet:?xt=urn:btih:C081C69939076EC6F0396B13AE926AB090AD8C39&dn=FNEW-009','_self')\">\r\n                \t<a style=\"color:#333\" rel=\"nofollow\" title=\"滑鼠右鍵點擊並選擇【複製連結網址】\" href=\"magnet:?xt=urn:btih:C081C69939076EC6F0396B13AE926AB090AD8C39&dn=FNEW-009\">\r\n                \t2025-04-26                \t</a>\r\n                </td>            \r\n            </tr>\r\n\t\t         \r\n            <tr onmouseover=\"this.style.backgroundColor='#F4F9FD';this.style.cursor='pointer';\" onmouseout=\"this.style.backgroundColor='#FFFFFF'\" height=\"35px\" style=\" border-top:#DDDDDD solid 1px\">\r\n                <td width=\"70%\" onclick=\"window.open('magnet:?xt=urn:btih:5454E9E0C327C76AD2D420632DDAE171CEFA63F4&dn=FNEW-009','_self')\">\r\n                \t<a style=\"color:#333\" rel=\"nofollow\" title=\"滑鼠右鍵點擊並選擇【複製連結網址】\" href=\"magnet:?xt=urn:btih:5454E9E0C327C76AD2D420632DDAE171CEFA63F4&dn=FNEW-009\">\r\n                \tFNEW-009                 \t</a>\r\n                </td>\r\n                <td style=\"text-align:center;white-space:nowrap\" onclick=\"window.open('magnet:?xt=urn:btih:5454E9E0C327C76AD2D420632DDAE171CEFA63F4&dn=FNEW-009','_self')\">\r\n                \t<a style=\"color:#333\" rel=\"nofollow\" title=\"滑鼠右鍵點擊並選擇【複製連結網址】\" href=\"magnet:?xt=urn:btih:5454E9E0C327C76AD2D420632DDAE171CEFA63F4&dn=FNEW-009\">\r\n                \t1.52GB                \t</a>\r\n                </td>\r\n                <td style=\"text-align:center;white-space:nowrap\" onclick=\"window.open('magnet:?xt=urn:btih:5454E9E0C327C76AD2D420632DDAE171CEFA63F4&dn=FNEW-009','_self')\">\r\n                \t<a style=\"color:#333\" rel=\"nofollow\" title=\"滑鼠右鍵點擊並選擇【複製連結網址】\" href=\"magnet:?xt=urn:btih:5454E9E0C327C76AD2D420632DDAE171CEFA63F4&dn=FNEW-009\">\r\n                \t2025-04-26                \t</a>\r\n                </td>            \r\n            </tr>\r\n\t\t         \r\n            <tr onmouseover=\"this.style.backgroundColor='#F4F9FD';this.style.cursor='pointer';\" onmouseout=\"this.style.backgroundColor='#FFFFFF'\" height=\"35px\" style=\" border-top:#DDDDDD solid 1px\">\r\n                <td width=\"70%\" onclick=\"window.open('magnet:?xt=urn:btih:A78DFDB729CA07CEDFCE10823AF8B098ADF79470&dn=fnew-009','_self')\">\r\n                \t<a style=\"color:#333\" rel=\"nofollow\" title=\"滑鼠右鍵點擊並選擇【複製連結網址】\" href=\"magnet:?xt=urn:btih:A78DFDB729CA07CEDFCE10823AF8B098ADF79470&dn=fnew-009\">\r\n                \tfnew-009 <a class=\"btn btn-mini-new btn-primary disabled\" title=\"包含高清HD的磁力連結\">高清</a>                \t</a>\r\n                </td>\r\n                <td style=\"text-align:center;white-space:nowrap\" onclick=\"window.open('magnet:?xt=urn:btih:A78DFDB729CA07CEDFCE10823AF8B098ADF79470&dn=fnew-009','_self')\">\r\n                \t<a style=\"color:#333\" rel=\"nofollow\" title=\"滑鼠右鍵點擊並選擇【複製連結網址】\" href=\"magnet:?xt=urn:btih:A78DFDB729CA07CEDFCE10823AF8B098ADF79470&dn=fnew-009\">\r\n                \t5.91GB                \t</a>\r\n                </td>\r\n                <td style=\"text-align:center;white-space:nowrap\" onclick=\"window.open('magnet:?xt=urn:btih:A78DFDB729CA07CEDFCE10823AF8B098ADF79470&dn=fnew-009','_self')\">\r\n                \t<a style=\"color:#333\" rel=\"nofollow\" title=\"滑鼠右鍵點擊並選擇【複製連結網址】\" href=\"magnet:?xt=urn:btih:A78DFDB729CA07CEDFCE10823AF8B098ADF79470&dn=fnew-009\">\r\n                \t2025-04-26                \t</a>\r\n                </td>            \r\n            </tr>\r\n\t\t  \r\n\t\t\t<script type=\"text/javascript\">\r\n\t\t\t$('#movie-loading').hide();\r\n\t\t\t</script>\r\n        "
-    //  根据 response.body 的内容，获取其中的magnet链接和大小信息，然后返回最大的那个磁链
-    
-    const magnetLinks = [...new Set(response.body.match(/magnet:\?xt=urn:btih:[A-F0-9]+&dn=[^&"']+/gi))]; // 获取并去重所有的magnet链接
-    const sizes = response.body.match(/\d+\.\d+GB|\d+MB/g); // 获取所有的大小信息
-    logger.info(`magnetLinks: ${magnetLinks} `);
-    logger.info(`sizes: ${sizes} `);
-    // 检查是否匹配到了磁链和大小信息
-    if (magnetLinks && sizes) {
-      const magnetSizePairs = magnetLinks.map((magnetLink, index) => ({
-        magnetLink,
-        size: sizes[index], // 对应大小信息
-      }));
-      logger.debug(`magnetSizePairs: ${magnetSizePairs} `);
-      // 解析大小信息为数字，单位为MB
-      const parsedMagnetSizePairs = magnetSizePairs.map((pair) => ({
-        magnetLink: pair.magnetLink,
-        size: parseFloat(pair.size.replace('GB', '').replace('MB', '')) * (pair.size.includes('GB') ? 1024 : 1), // 转换为MB
+    const magnetLinks = [...new Set(response.body.match(/magnet:\?xt=urn:btih:[A-F0-9]+&dn=[^&"']+/gi))];
+    const sizes = response.body.match(/\d+\.\d+GB|\d+MB/g);
 
-      }))
-      logger.debug(`parsedMagnetSizePairs: ${parsedMagnetSizePairs} `);
-      // 找到最大的磁链
-      const maxSizePair = parsedMagnetSizePairs.reduce((max, current) => {
-        return current.size > max.size? current : max;
-      }, { size: 0 });
-      logger.debug(`maxSizePair: ${maxSizePair} `);
-      return maxSizePair.magnetLink; // 返回最大的磁链
-    } else {
-      return null; // 如果没有匹配到磁链和大小信息，返回null
-    }
-    
-    
-    // const $ = require('cheerio').load(response.body);
+    if (!magnetLinks || !sizes) return null;
 
+    const parsedPairs = magnetLinks.map((magnetLink, index) => {
+      const sizeStr = sizes[index];
+      const sizeValue = parseFloat(sizeStr.replace(/GB|MB/, ''));
+      const sizeInMB = sizeStr.includes('GB') ? sizeValue * 1024 : sizeValue;
+      return { magnetLink, size: sizeInMB };
+    });
 
-
-
-    // const magnetLinks = $('tr')
-    //   .map((i, row) => {
-    //     const magnetLink = $(row).find('td a').first().attr('href'); // 获取第一个 <a> 元素的 href 属性
-    //     const sizeText = $(row).find('td').eq(1).text().trim(); // 获取第二个 <td> 元素的文本内容，并去除首尾空格
-    //     const sizeMatch = /([\d.]+)(GB|MB)/i.exec(sizeText);
-    //     logger.debug(`magnetLink: ${magnetLink} `);
-    //     logger.info(`sizeText: ${sizeText} `);
-    //     logger.info(`sizeMatch: ${sizeMatch} `);
-    //     // 检查是否匹配到了大小信息
-    //     if (magnetLink && sizeMatch) {
-    //       const size = parseFloat(sizeMatch[1]);
-    //       const unit = sizeMatch[2].toUpperCase();
-    //       return {
-    //         magnetLink,
-    //         size: unit === 'GB' ? size * 1024 : size, // Convert GB to MB for comparison
-    //       };
-    //     }
-    //     return null;
-    //   })
-    //   .get()
-    //   .filter(Boolean)
-    //   .sort((a, b) => b.size - a.size)
-    //   .map(item => item.magnetLink);
-    // return magnetLinks.length > 0 ? magnetLinks : null;
-
+    return parsedPairs.reduce((max, current) =>
+      current.size > max.size ? current : max,
+      { size: 0 }
+    ).magnetLink;
   }
 
 }
-
 
 module.exports = Parser;
