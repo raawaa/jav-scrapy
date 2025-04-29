0805476983233ea13a607d21f25305248909a794
raawaa <wenjie_yu@yeah.net>
2025-04-29 14:28:26 +0800

    refactor: 移除命令行选项的默认值参数
    移除 `-p, --parallel` 和 `-l, --limit` 选项的默认值参数，以简化代码并保持一致性

--- src/jav.ts
+++ src/jav.ts
@@ -15,9 +15,9 @@ import { Config } from './types/interfaces';
 program
     .version(version)
     .usage('[options]')
-    .option('-p, --parallel <num>', '设置抓取并发连接数，默认值：2', '2')
+    .option('-p, --parallel <num>', '设置抓取并发连接数，默认值：2')
     .option('-t, --timeout <num>', '自定义连接超时时间(毫秒)。默认值：30000毫秒')
-    .option('-l, --limit <num>', '设置抓取影片的数量上限，0为抓取全部影片。默认值：0', '0')
+    .option('-l, --limit <num>', '设置抓取影片的数量上限，0为抓取全部影片。默认值：0')
     .option('-o, --output <file_path>', '设置磁链和封面抓取结果的保存位置，默认为当前用户的主目录下的 magnets 文件夹')
     .option('-s, --search <string>', '搜索关键词，可只抓取搜索结果的磁链或封面')
     .option('-b, --base <url>', '自定义抓取的起始页')
@@ -74,7 +74,7 @@ class JavScraper {
         }
 
     }
-   
+
 }
 
 
